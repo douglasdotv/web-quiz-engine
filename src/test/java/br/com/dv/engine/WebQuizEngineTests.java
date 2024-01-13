b2a923b74310a4f5343f92f28848e31d1f3cddd9
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-12 22:58:51 -0300

    Initial commit

---
+++ src/test/java/br/com/dv/engine/WebQuizEngineTests.java
@@ -0,0 +1,13 @@
+package br.com.dv.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizEngineTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
