07d446a700c028468aef712a03a226ceb068da80
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-13 22:45:54 -0300

    (2): create QuizRequest DTO

---
+++ src/main/java/br/com/dv/engine/dto/QuizRequest.java
@@ -0,0 +1,14 @@
+package br.com.dv.engine.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record QuizRequest(
+        String title,
+        String text,
+        List<String> options,
+        @JsonProperty("answer")
+        Integer answerIndex
+) {
+}
