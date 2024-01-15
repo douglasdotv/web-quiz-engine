b325c3d2bcaac37e3ab2a0b9fed815c8168fe3d1
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-15 00:30:52 -0300

    (3): update quiz addition logic to support multiple answer indices
    Updated service layer to handle multiple answer indices as per new QuizRequest DTO

--- src/main/java/br/com/dv/engine/service/QuizServiceImpl.java
+++ src/main/java/br/com/dv/engine/service/QuizServiceImpl.java
@@ -8,8 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -19,6 +18,7 @@ public class QuizServiceImpl implements QuizService {
     private static final int INITIAL_QUIZ_ID = 1;
     private static final String CORRECT_FEEDBACK = "Congratulations, you're right!";
     private static final String INCORRECT_FEEDBACK = "Wrong answer! Please try again.";
+    private static final String DUPLICATE_ANSWER_INDICES_MESSAGE = "Duplicate answer indices are not allowed.";
     private static final String QUIZ_NOT_FOUND_MESSAGE = "Quiz not found.";
 
     private final AtomicInteger idCounter = new AtomicInteger(INITIAL_QUIZ_ID);
@@ -44,13 +44,15 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public QuizResponse addQuiz(QuizRequest quiz) {
+        Set<Integer> uniqueAnswerIndices = getUniqueAnswerIndices(quiz.answerIndices());
+
         Quiz newQuiz = new Quiz();
 
         newQuiz.setId(idCounter.getAndIncrement());
         newQuiz.setTitle(quiz.title());
         newQuiz.setText(quiz.text());
         newQuiz.setOptions(quiz.options());
-        newQuiz.setAnswerIndex(quiz.answerIndex());
+        newQuiz.setAnswerIndices(uniqueAnswerIndices);
 
         quizzes.put(newQuiz.getId(), newQuiz);
 
@@ -68,6 +70,17 @@ public class QuizServiceImpl implements QuizService {
         }
 
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, QUIZ_NOT_FOUND_MESSAGE);
+    }
+
+    private Set<Integer> getUniqueAnswerIndices(List<Integer> answerIndices) {
+        if (answerIndices != null) {
+            Set<Integer> uniqueAnswerIndices = new HashSet<>(answerIndices);
+            if (uniqueAnswerIndices.size() != answerIndices.size()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, DUPLICATE_ANSWER_INDICES_MESSAGE);
+            }
+            return uniqueAnswerIndices;
+        }
+        return Collections.emptySet();
     }
 
 }
