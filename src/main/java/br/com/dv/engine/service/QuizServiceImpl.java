aeaafc29bd4928112481821b2aa1bfeb21c6e08b
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-15 01:25:15 -0300

    (3): update service layer to use custom exceptions
    Replaced ResponseStatusException usage with custom exceptions (part 3/3 of the API exception handling refactoring process)

--- src/main/java/br/com/dv/engine/service/QuizServiceImpl.java
+++ src/main/java/br/com/dv/engine/service/QuizServiceImpl.java
@@ -4,10 +4,10 @@ import br.com.dv.engine.dto.AnswerSubmissionRequest;
 import br.com.dv.engine.dto.AnswerSubmissionResponse;
 import br.com.dv.engine.dto.QuizRequest;
 import br.com.dv.engine.dto.QuizResponse;
+import br.com.dv.engine.exception.DuplicateAnswerIndicesException;
+import br.com.dv.engine.exception.QuizNotFoundException;
 import br.com.dv.engine.model.Quiz;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,8 +19,6 @@ public class QuizServiceImpl implements QuizService {
     private static final int INITIAL_QUIZ_ID = 1;
     private static final String CORRECT_FEEDBACK = "Congratulations, you're right!";
     private static final String INCORRECT_FEEDBACK = "Wrong answer! Please try again.";
-    private static final String DUPLICATE_ANSWER_INDICES_MESSAGE = "Duplicate answer indices are not allowed.";
-    private static final String QUIZ_NOT_FOUND_MESSAGE = "Quiz not found.";
 
     private final AtomicInteger idCounter = new AtomicInteger(INITIAL_QUIZ_ID);
     private final Map<Integer, Quiz> quizzes = new ConcurrentHashMap<>();
@@ -40,7 +38,7 @@ public class QuizServiceImpl implements QuizService {
             return new QuizResponse(quiz.getId(), quiz.getTitle(), quiz.getText(), quiz.getOptions());
         }
 
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, QUIZ_NOT_FOUND_MESSAGE);
+        throw new QuizNotFoundException();
     }
 
     @Override
@@ -72,14 +70,14 @@ public class QuizServiceImpl implements QuizService {
             return new AnswerSubmissionResponse(isCorrect, feedback);
         }
 
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, QUIZ_NOT_FOUND_MESSAGE);
+        throw new QuizNotFoundException();
     }
 
     private Set<Integer> getUniqueAnswerIndices(List<Integer> answerIndices) {
         if (answerIndices != null) {
             Set<Integer> uniqueAnswerIndices = new HashSet<>(answerIndices);
             if (uniqueAnswerIndices.size() != answerIndices.size()) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, DUPLICATE_ANSWER_INDICES_MESSAGE);
+                throw new DuplicateAnswerIndicesException();
             }
             return uniqueAnswerIndices;
         }
