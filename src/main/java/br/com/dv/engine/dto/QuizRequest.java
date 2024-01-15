420b5dddc4b50f304fb08c8c7bd396dfe2996873
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-15 00:19:10 -0300

    (3): enhance QuizRequest DTO with field validation

--- src/main/java/br/com/dv/engine/dto/QuizRequest.java
+++ src/main/java/br/com/dv/engine/dto/QuizRequest.java
@@ -1,12 +1,19 @@
 package br.com.dv.engine.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record QuizRequest(
+        @NotBlank
         String title,
+        @NotBlank
         String text,
+        @NotNull
+        @Size(min = 2)
         List<String> options,
         @JsonProperty("answer")
         List<Integer> answerIndices
