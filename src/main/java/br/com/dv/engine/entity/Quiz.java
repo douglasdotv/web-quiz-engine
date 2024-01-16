b4df9ae0881f086dc2327c1c61e06e0018da528b
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-15 23:50:00 -0300

    (4): create Quiz entity and replace existing Quiz model

---
+++ src/main/java/br/com/dv/engine/entity/Quiz.java
@@ -0,0 +1,33 @@
+package br.com.dv.engine.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Quiz {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    private String title;
+
+    private String text;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Fetch(value = FetchMode.SUBSELECT)
+    private List<String> options;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Fetch(value = FetchMode.SUBSELECT)
+    private Set<Integer> answerIndices;
+
+}
