2615f2f55727d848c486844e6b349d619c5e59cf
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-16 19:52:05 -0300

    (5): add findByEmail method to AppUser repository

--- src/main/java/br/com/dv/engine/repository/AppUserRepository.java
+++ src/main/java/br/com/dv/engine/repository/AppUserRepository.java
@@ -3,5 +3,10 @@ package br.com.dv.engine.repository;
 import br.com.dv.engine.entity.AppUser;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+
+    Optional<AppUser> findByEmail(String email);
+
 }
