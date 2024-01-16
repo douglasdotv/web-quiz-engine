85a24e3e8a0a135b2135d41f1f6adfaeac542bf4
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-15 23:58:00 -0300

    (4): integrate Quiz repository in Quiz service for data persistence
     Integrated `QuizRepository` in `QuizServiceImpl` for storing and retrieving quiz data.
    - Transitioned from in-memory structures to database persistence.
    - Updated the existing QuizServiceImpl methods accordingly.

--- src/main/java/br/com/dv/engine/service/QuizServiceImpl.java
+++ src/main/java/br/com/dv/engine/service/QuizServiceImpl.java
@@ -4,37 +4,45 @@ import br.com.dv.engine.dto.AnswerSubmissionRequest;
 import br.com.dv.engine.dto.AnswerSubmissionResponse;
 import br.com.dv.engine.dto.QuizRequest;
 import br.com.dv.engine.dto.QuizResponse;
+import br.com.dv.engine.entity.Quiz;
 import br.com.dv.engine.exception.DuplicateAnswerIndicesException;
 import br.com.dv.engine.exception.QuizNotFoundException;
-import br.com.dv.engine.model.Quiz;
+import br.com.dv.engine.repository.QuizRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.StreamSupport;
 
 @Service
 public class QuizServiceImpl implements QuizService {
 
-    private static final int INITIAL_QUIZ_ID = 1;
     private static final String CORRECT_FEEDBACK = "Congratulations, you're right!";
     private static final String INCORRECT_FEEDBACK = "Wrong answer! Please try again.";
 
-    private final AtomicInteger idCounter = new AtomicInteger(INITIAL_QUIZ_ID);
-    private final Map<Integer, Quiz> quizzes = new ConcurrentHashMap<>();
+    private final QuizRepository quizRepository;
+
+    public QuizServiceImpl(QuizRepository quizRepository) {
+        this.quizRepository = quizRepository;
+    }
 
     @Override
+    @Transactional(readOnly = true)
     public List<QuizResponse> getAllQuizzes() {
-        return quizzes.values().stream()
+        Iterable<Quiz> quizzes = quizRepository.findAll();
+
+        return StreamSupport.stream(quizzes.spliterator(), false)
                 .map(quiz -> new QuizResponse(quiz.getId(), quiz.getTitle(), quiz.getText(), quiz.getOptions()))
                 .toList();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public QuizResponse getQuizById(Integer id) {
-        Quiz quiz = quizzes.get(id);
+        Optional<Quiz> quizOptional = quizRepository.findById(id);
 
-        if (quiz != null) {
+        if (quizOptional.isPresent()) {
+            Quiz quiz = quizOptional.get();
             return new QuizResponse(quiz.getId(), quiz.getTitle(), quiz.getText(), quiz.getOptions());
         }
 
@@ -42,31 +50,36 @@ public class QuizServiceImpl implements QuizService {
     }
 
     @Override
+    @Transactional
     public QuizResponse addQuiz(QuizRequest quiz) {
         Set<Integer> uniqueAnswerIndices = getUniqueAnswerIndices(quiz.answerIndices());
 
         Quiz newQuiz = new Quiz();
 
-        newQuiz.setId(idCounter.getAndIncrement());
         newQuiz.setTitle(quiz.title());
         newQuiz.setText(quiz.text());
         newQuiz.setOptions(quiz.options());
         newQuiz.setAnswerIndices(uniqueAnswerIndices);
 
-        quizzes.put(newQuiz.getId(), newQuiz);
+        quizRepository.save(newQuiz);
 
         return new QuizResponse(newQuiz.getId(), newQuiz.getTitle(), newQuiz.getText(), newQuiz.getOptions());
     }
 
     @Override
+    @Transactional(readOnly = true)
     public AnswerSubmissionResponse submitAnswer(Integer id, AnswerSubmissionRequest answer) {
-        Quiz quiz = quizzes.get(id);
+        Optional<Quiz> quizOptional = quizRepository.findById(id);
 
-        if (quiz != null) {
+        if (quizOptional.isPresent()) {
+            Quiz quiz = quizOptional.get();
+
             Set<Integer> correctAnswerIndices = quiz.getAnswerIndices();
             Set<Integer> submittedAnswerIndices = getUniqueAnswerIndices(answer.answerIndices());
+
             boolean isCorrect = verifyAnswer(correctAnswerIndices, submittedAnswerIndices);
             String feedback = isCorrect ? CORRECT_FEEDBACK : INCORRECT_FEEDBACK;
+
             return new AnswerSubmissionResponse(isCorrect, feedback);
         }
 
