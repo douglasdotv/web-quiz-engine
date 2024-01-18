d4d89a302b654aeb16dd2ed90a3a2a630446540e
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-18 10:09:44 -0300

    (6): create QuizCompletion repository

---
+++ src/main/java/br/com/dv/engine/repository/QuizCompletionRepository.java
@@ -0,0 +1,7 @@
+package br.com.dv.engine.repository;
+
+import br.com.dv.engine.entity.QuizCompletion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuizCompletionRepository extends CrudRepository<QuizCompletion, Integer> {
+}
