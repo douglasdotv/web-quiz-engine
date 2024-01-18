8af86d6da9ce4dd43c4eb4a155511e8fd95de973
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-01-18 12:33:30 -0300

    (6): format completedAt field date and time as per requirements

--- src/main/java/br/com/dv/engine/dto/CompletedQuizResponse.java
+++ src/main/java/br/com/dv/engine/dto/CompletedQuizResponse.java
@@ -1,6 +1,12 @@
 package br.com.dv.engine.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record CompletedQuizResponse(Integer id, LocalDateTime completedAt) {
+public record CompletedQuizResponse(
+        Integer id,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+        LocalDateTime completedAt
+) {
 }
